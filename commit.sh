631c27afe2f50fe414b3f1b6a7f18a88b92dbe20
LailaMartinez <>
2021-04-08 19:46:43 +0000

    second commit

---
+++ commit.sh
@@ -0,0 +1,7 @@
+git init
+git add --all
+git config --global user.name "lmtnz"
+git config --global user.email "lmartinez140@gmail.com"
+git commit -m "second commit"
+git remote add origin https://github.com/lmtnz/lmtnz.github.io.git
+git push -u origin master 
